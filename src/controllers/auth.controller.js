d70a289ad5965877017c691035d162230abd8caf
Sideline <sidelinewebs@gmail.com>
2023-11-05 18:01:57 -0300

    up

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,6 @@ import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-
   try {
     const user = await loginService(email);
 
