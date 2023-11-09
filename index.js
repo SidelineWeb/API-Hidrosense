5fff0659592aa7dc62471809557ad92fd8fd758a
Sideline <sidelinewebs@gmail.com>
2023-11-09 10:51:19 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDatabase from "./src/database/dbmongo.js";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
@@ -11,6 +12,23 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+// Permitir todos os domínios (não seguro para produção)
+app.use(cors());
+
+/* Ou se quiser permitir apenas alguns domínios específicos:
+const whitelist = ['http://localhost:3000', 'https://outro-dominio.com'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));*/
 
 connectDatabase();
 app.use(express.json());
